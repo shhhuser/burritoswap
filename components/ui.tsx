50f72b8b0fd7d3ba12377fb506bcaf738b45d3de
MoChain Official <qazimishpk@gmail.com>
2025-08-30 01:19:56 +0200

    Update ui.tsx

--- components/ui.tsx
+++ components/ui.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { motion } from "framer-motion";
 
 export function PageContainer({ children }: { children: React.ReactNode }) {
   return <div className="w-full max-w-5xl mx-auto px-4 md:px-6">{children}</div>;
